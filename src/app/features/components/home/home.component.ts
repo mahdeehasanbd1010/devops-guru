bc3747fa6f6764e13216ef11e88a59ec65d55250
mahdeehasanbd1010 <bsse1010@iit.du.ac.bd>
2022-08-05 03:51:27 +0600

    add others routing

--- src/app/features/components/home/home.component.ts
+++ src/app/features/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
   
+  routeToCourseSection(){
+    this.router.navigate(["admin/course"]);
+  }
+
+  routeToStudentSection(){
+    this.router.navigate(["admin/student"]);
+  }
 
 }
